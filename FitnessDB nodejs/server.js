222bba94960b6bb8aacb6ecd30e601cc6b731552
Spliomix <spliomixx@gmail.com>
2017-12-21 17:28:29 +0100

    serverjs with the database conn
    tested

--- FitnessDB nodejs/server.js
+++ FitnessDB nodejs/server.js
@@ -8,7 +8,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var url = require('url');
-
+var id;
+var oracledb = require('oracledb');
+oracledb.maxRows = 10000;
+var user;
+var pwd;
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -17,47 +21,25 @@ app.set('view engine', '.hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 
-var gegessen1={ID:"1", gname:"Nudeln", datum:"01-Jan-2017", fett:"33", eiweis:"55", Kohlenhydrate:"10", brennwert: "500"};
-var gegessen2={ID:"1", gname:"Nudeln", datum:"02-Jan-2017", fett:"33", eiweis:"55", Kohlenhydrate:"10", brennwert:"1234"};
-var gegessen=[gegessen1, gegessen2];
 
-var gericht = {gname:"Nudeln", fett:"500", protein:"300", Kohlenhydrate:"100", brennwert: 150};
-var gericht2 = {gname:"Mais", fett:"5001", protein:"3010", Kohlenhydrate:"1100", brennwert: 1510};
-var gerichte= [gericht, gericht2];
+
+
+
+
 
 app.get('/', function(req, res, next) {
-  res.render(__dirname + '/views/index.hbs');
-});
-
-app.get('/main', function(req, res, next) {
-  res.render(__dirname + '/views/main.hbs');
-});
-
-app.get('/Employe', function(req, res, next) {
-  res.render(__dirname + '/views/addEmploye.hbs',{members: members});
-});
-
-
-var trainiert1={ID:"133", name:"Bankdrücken", datum:"01-Jan-2017", kathegorie:"Brust", reps:"55", satz:"10", gewicht: "500"};
-var trainiert2={ID:"11", name:"Latzug", datum:"01-Jan-2017", kathegorie:"Brust", reps:"55", satz:"10", gewicht: "500"};
-var trainiert=[trainiert1, trainiert2];
-
-app.get('/overviewTrain', function(req, res, next) {
+    res.render(__dirname + '/views/index.hbs');
+  });
   
-  //get Food Data from the isst table with the id in the url
-  console.log("The ID is:" + url.parse(req.url, true).search);
+  app.get('/main', function(req, res, next) {
+    res.render(__dirname + '/views/main.hbs');
+  });
   
-  res.render(__dirname + '/views/overviewTrain.hbs', {training: trainiert});
-});
-
+ 
 
 app.get('/Optionen', function(req, res, next) {
   res.render(__dirname + '/views/main.hbs');
@@ -67,55 +49,357 @@ app.get('/profil', function(req, res, next) {
   res.render(__dirname + '/views/profil.hbs');
 });
 
-app.get('/overviewFood', function(req, res, next) {
-  
-  //get Food Data from the isst table with the id in the url
-  console.log("The ID is:" + url.parse(req.url, true).search);
-  
-  res.render(__dirname + '/views/overviewFood.hbs', {food: gegessen});
-});
 
-var member1 = {id:"134", vorname:"luffi", nachname:"brazzer", gewicht:"100", anmeldedatum: 150, punkte:11, passwort:"sdfsdfsmggg", emopholen:""};
-var member2 = {id:"22", vorname:"bros", nachname:"Dragan", gewicht:"45", anmeldedatum: 150, punkte:233, passwort:"sd2222222gg", emopholen:""};
-var members= [member1, member2,member1, member2,member1, member2,member1, member2];
-app.get('/Member', function(req, res, next) {
-  res.render(__dirname + '/views/addMember.hbs', {members: members});
-});
-app.post('/saveNewMember', function(req, res, next){
-  console.log(req.body.vorname);
-  //save body things in the databse
-  //res.render(__dirname + '/views/addMember.hbs', {members: members});
-});
 
 
 app.post('/ajax', (req,res)=>{
-    console.log(req.body)  //your variables are here.
-    res.status(200).json({msg:'OK'});
-  ////////////save these vars in the database
-  req.body.vorname;
-  req.body.nachname;
-  req.body.pwd;
-  req.body.gewicht;
-  req.body.empfohlen;
+    
+  req.body.empfohlen;//check ob vorhanden
   var datetime = new Date();//change to oracle Format
 
+  var liste = [null, req.body.vorname, req.body.nachname,req.body.gewicht];
+  DbConnectionlol("INSERT INTO menschen VALUES (:id, :vorname, :nachname, :gewicht)",liste,
+      function(err, result){
+      if(!err){
+          console.log(result.rowsAffected);//if 1 ->succsesfull
+      }
+  });
+	setTimeout(function() {
+        console.log(req.body.empfohlen);
+        if (req.body.empfohlen.length == 0)
+	        empfohlen=null;
+        else
+            empfohlen=req.body.empfohlen;
 
+	    var mitglied = [id, actDate(), 100, req.body.pwd, empfohlen];
+        DbConnectionlol("INSERT INTO mitglied VALUES (:mid, :anemdledatum, :punkte, :passwort, :empfohlen)",
+            mitglied,
+	        function(err, result){
+	            if(!err){
+		            console.log(result.rowsAffected);//if 1 ->succsesfull
+		            if (result.rowsAffected) res.status(200).json({msg:'OK'});
+		            else res.status(200).json({msg:'NOK'});
+	            }
+	        });
+	}, 1000);
 });
 
 
 
 app.post('/ajax/searchMember', (req,res)=>{
-    console.log(req.body)  //your variables are here.
-  
-    res.json({smembers:members});
-  ////////////search these vars in the database
-  req.body.vorname;
-  req.body.nachname;
-  var datetime = new Date();//change to oracle Format
+    var members=[];
+    var searchopt=[req.body.vorname, req.body.nachname];
+    DbConnectionlol("SELECT * FROM view_member_node WHERE vorname=:vorname or nachname=:nachname ",searchopt, function(err, result){
+        if(!err){
+            console.log(result.rows[0][0]); 
+	        for( i = 0; i<result.rows.length;i++){
+                    members[i]={placeholder:"0"};
+                    members[i]["id"]=result.rows[i][0];
+                    members[i]["anmeldedatum"]=result.rows[i][1];
+                    members[i]["punkte"]=result.rows[i][2];
+                    members[i]["passwort"]=result.rows[i][3];
+                    members[i]["emfpholen"]=result.rows[i][4];//5 wäre nochmal die id
+                    members[i]["vorname"]=result.rows[i][6];
+                    members[i]["nachname"]=result.rows[i][7];
+                    members[i]["gewicht"]=result.rows[i][8];
+            }res.json({smembers:members});
+        }    
+  });
 });
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+
+
+
+
+
+
+
+/////////////////////////
+/////////////////////
+/////           Training ///////
+////////////////////////////////
+app.get('/overviewTrain', function(req, res, next) {
+    var trainiert=[];
+    var id=url.parse(req.url, true).search;
+    id=id.substring(1);
+    DbConnection("SELECT * FROM training WHERE id=:id",id, function(err, result){
+	    if(!err){
+            console.log(result)
+            for(i = 0; i<result.length;i++){
+                trainiert[i]={placeholder:"0"};
+                trainiert[i]["id"]=result[i][0];
+                trainiert[i]["datum"]=result[i][1];
+                trainiert[i]["name"]=result[i][2];
+                trainiert[i]["gewicht"]=result[i][3];
+                trainiert[i]["reps"]=result[i][4];
+                trainiert[i]["kathegorie"]=result[i][5];
+            }
+	    }res.render(__dirname + '/views/overviewTrain.hbs', {training: trainiert,id:id});
+    });
+  
+});
+
+
+
+///////////////////////
+////////////////////////////////////
+/////////////       Food            /////////////////////
+//////////////////////////
+
+app.get('/overviewFood', function(req, res, next) {
+    
+    var id=url.parse(req.url, true).search;
+    id=id.substring(1);
+    var gegessen=[];
+    DbConnection("SELECT * FROM isst WHERE id=:id", id, function(err, result){
+        if(!err){
+            console.log(result);
+            for(i = 0; i<result.length;i++){
+                    gegessen[i]={placeholder:"0"};
+                    gegessen[i]["id"]=result[i][0];
+                    gegessen[i]["name"]=result[i][1];
+                    gegessen[i]["datum"]=result[i][2];
+                    gegessen[i]["menge"]=result[i][3];
+            }
+        }   res.render(__dirname + '/views/overviewFood.hbs', {food: gegessen,id:id});
+    });
+ 
+});
+
+
+
+
+
+
+///////////////////////////////
+//////////// Hinzufügen eines Mitarbeiters
+///////////////////////////
+////////////////////
+/*
+var liste = [null, 'Chris', 'Un',45];
+DbConnectionlol("INSERT INTO menschen VALUES (:id, :vorname, :nachname, :gewicht)",liste,
+	function(err, result){
+	if(!err){
+		console.log(result.rowsAffected);//if 1 ->succsesfull
+	}
+});
+
+var mitarbeiter = [id, 5, '7654'];
+DbConnectionlol("INSERT INTO mitarbeiter VALUES (:mid, :rang, :personalnummer)",mitarbeiter,
+	function(err, result){
+	if(!err){
+		console.log(result.rowsAffected);//if 1 ->succsesfull
+	}
+}); 
+
+*/
+
+
+
+
+//////////////////
+//////////////////////
+//////Mitarbeiter////////////////////////////
+////////////////////////////////////
+app.get('/Employe', function(req, res, next) {
+    var members=[];
+    DbConnectionSAll("SELECT * FROM view_employe_node", function(err, result){//not testet
+        if(!err){
+                for(i = 0; i<result.length;i++){
+                    members[i]={placeholder:"0"};
+                    members[i]["id"]=result[i][0];
+                    members[i]["rang"]=result[i][1];
+                    members[i]["personalnummer"]=result[i][2];//3 wäre nochmal die id
+                    members[i]["vorname"]=result[i][4];
+                    members[i]["nachname"]=result[i][5];
+                    members[i]["gewicht"]=result[i][6];
+                }
+        }res.render(__dirname + '/views/addEmploye.hbs',{members: members});
+    });
+
+  });
+
+
+
+
+
+
+////////////////////////////////////////
+//////////////////////////////
+//Mitglieder ALL
+/////////////////////////////////
+app.get('/Member', function(req, res, next) {
+    var members=[];
+    DbConnectionSAll("SELECT * FROM view_member_node", function(err, result){      
+        if(!err){
+            //algo
+                for( i = 0; i<result.length;i++){
+                    members[i]={placeholder:"0"};
+                    members[i]["id"]=result[i][0];
+                    members[i]["anmeldedatum"]=result[i][1];
+                    members[i]["punkte"]=result[i][2];
+                    members[i]["passwort"]=result[i][3];
+                    members[i]["emfpholen"]=result[i][4];//5 wäre nochmal die id
+                    members[i]["vorname"]=result[i][6];
+                    members[i]["nachname"]=result[i][7];
+                    members[i]["gewicht"]=result[i][8];
+                }
+        }res.render(__dirname + '/views/addMember.hbs', {members: members});
+    });  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DbConnection = function (query, name, callback){
+	oracledb.getConnection(
+	 	{
+		   connectString : "oracle-lab.cs.univie.ac.at/LAB.CS.UNIVIE.AC.AT",
+		   user : user,
+		   password : pwd
+	 	},
+	 	function(err, connection){
+            if(err){console.log(err.message);return;}
+		    connection.execute(
+            	query,
+                [name],
+                function(err, result)
+                {
+                    if (err) { console.error(err.message); return; }
+                    else if(result){
+                        return callback(null, result.rows);
+                    }
+               
+                    connection.close(
+                        function(err){
+                            if (err) { console.error(err.message); }
+                        }); 
+		        }
+            );
+        }
+    );
+};
+
+
+
+DbConnectionSAll = function (query, callback){
+	oracledb.getConnection(
+	 	{
+		   connectString : "oracle-lab.cs.univie.ac.at/LAB.CS.UNIVIE.AC.AT",
+		   user : user,
+		   password : pwd
+	 	},
+	 	function(err, connection){
+            if(err){console.log(err.message);return;}
+		    connection.execute(
+            	query,
+               	function(err, result){
+                    	if (err) { console.error(err.message); return; }
+                    	else if(result){
+                       		return callback(null, result.rows);
+                        }    
+                                
+			            setTimeout(function() {
+			                connection.close(
+		      		            function(err){
+			                        if (err) { console.error(err.message); }
+                                  }); 
+                        }, 3000);
+		        }
+            );
+        }
+    );
+};
+
+
+DbConnectionlol = function (query, list, callback){
+	oracledb.getConnection(
+	 	{
+		   connectString : "oracle-lab.cs.univie.ac.at/LAB.CS.UNIVIE.AC.AT",
+		   user : user,
+		   password : pwd
+	 	},
+	 	function(err, connection){
+            if(err){console.log(err.message);return;}
+		    connection.execute(
+            	query,
+                list,
+		        {autoCommit: true},
+                function(err, result)
+                {
+                    if (err) { console.error(err.message); return; }
+                    else if(result){
+                        return callback(null, result);
+                    }
+                }
+            );
+            connection.execute(
+                "SELECT sequenz_id.currval FROM dual",
+                function(err, res)
+                {
+                    if (err) {
+                        return; 
+                    }else if(res){
+                        id=res.rows[0][0];
+                        console.log("Neues id:"+ res.rows[0]);
+                    }
+                    setTimeout(function() {
+			            connection.close(
+		      		        function(err){
+			                    if (err) { console.error(err.message); }
+                              });
+                        }, 3000);
+                }
+            );
+        }
+    );
+}
+
+actDate= function(){// GET CURRENT DATE
+	var date = new Date();
+	 
+	// GET YYYY, MM AND DD FROM THE DATE OBJECT
+	var yyyy = date.getFullYear().toString();
+	var mm = (date.getMonth()+1).toString();
+	var dd  = date.getDate().toString();
+	 
+	// CONVERT mm AND dd INTO chars
+	var mmChars = mm.split('');
+	var ddChars = dd.split('');
+	 
+	var month = new Array();
+	month[0] = "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+
+	// CONCAT THE STRINGS IN YYYY-MM-DD FORMAT
+	var datestring = (ddChars[1]?dd:"0"+ddChars[0])+'-'+ month[date.getMonth()] + '-' +  yyyy;
+	return datestring;
+}
+
+
+var listener = app.listen(3000, function(){
+   console.log('Yoour app is listening on port' + 3000);
 });
